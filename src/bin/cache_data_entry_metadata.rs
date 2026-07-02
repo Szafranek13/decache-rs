ca5192d2dd9d340b6894f52f2cdd2bb7ed03ef93
Jakub Fabijan <jakubfabijan@tuta.io>
2026-07-02 16:47:24 +0200

    Started writing chromium's cache parser

--- src/bin/cache_data_entry_metadata.rs
+++ src/bin/cache_data_entry_metadata.rs
@@ -1,45 +1,62 @@
-//There's no rust library that would parse it for me, fuck my faggy ass again I guess...
-//I should probably turn those cache parsers into real rust crates,
-//maybe it will help other people with their projects instead of going trough the same hell as I do
+//The only parser crate there is for this shit doesn't work and hasn't been updated in the last 9 months. Fuck my faggy ass again I guess >:(
 
-//the-real-index is little endian
-//it contains list of cached entries with lenght of their metadata i guess?
 
-//use std::fs::File;
-//use std::io::{self, Read, Seek, SeekFrom};
+use std::{
+    fs::File,
+    io::{self, Read, Seek, SeekFrom},
+};
 
-//fn find(data: &[u8], needle: &[u8]) -> Option<usize> {
-//    data.windows(needle.len())
-//        .position(|window| window == needle)
-//}
-//
-//fn find_double_null(data: &[u8]) -> Option<usize> {
-//    data.windows(2)
-//        .position(|window| window == [0, 0])
-//}
-//
-//fn main() -> Result<(), Box<dyn std::error::Error>> {
-//    let data = fs::read(
-//        "/home/jakub/.cache/chromium/Default/Cache/Cache_Data/1b554cc6b572dd47_0"
-//    )?;
-//
-//    if let Some(pos) = find(&data, b"HTTP/1.1") {
-//        println!("HTTP headers found at offset {}", pos);
-//
-//        let end = find_double_null(&data[pos..])
-//            .unwrap_or(data.len() - pos);
-//
-//        let headers = &data[pos..pos + end];
-//
-//        for part in headers.split(|b| *b == 0) {
-//            if !part.is_empty() {
-//                println!("{}", String::from_utf8_lossy(part));
-//            }
-//        }
-//    } else {
-//        println!("No HTTP headers found");
-//    }
-//
-//    Ok(())
-//}
-fn main() {}
+/// Magick number at the beggining of every Chromium cache entry file
+const CHROMIUM_CACHE_ENTRY_MAGICK: u32 = u32::from_le_bytes([0x30, 0x5C, 0x72, 0xA7]);
+
+#[derive(Debug)]
+struct EntryHeader {f
+    magick: u32,
+    version: u32,
+    key_len: u32,
+    key_hash: u32,
+    stream_sizes: [u32; 4],
+}
+
+fn read_u32_le<R: Read>(reader: &mut R) -> io::Result<u32> {
+    let mut buf = [0; 4];
+    reader.read_exact(&mut buf)?;
+    Ok(u32::from_le_bytes(buf))
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("/home/jakub/.cache/chromium/Default/Cache/Cache_Data/0340d4a5082cbe60_0")?;
+    //Get magick filetype
+    let magick = read_u32_le(&mut file)?;
+    let version = read_u32_le(&mut file)?;
+    let key_len = read_u32_le(&mut file)?; // TODO: FIX IT, IT HAS TO BE WRONG
+    let key_hash = read_u32_le(&mut file)?;
+
+    let mut stream_sizes = [0u32; 4];
+    for size in &mut stream_sizes { //TODO: Find out what are those. There's no way size of stream_0 is 5 bytes
+        *size = read_u32_le(&mut file)?;
+    }
+
+    if magick != CHROMIUM_CACHE_ENTRY_MAGICK {
+        return Err("Not a Chromium cache entry file".into());
+    }
+
+    let fag = EntryHeader{
+        magick: magick,
+        version: version,
+        key_len: key_len,
+        key_hash: key_hash,
+        stream_sizes: stream_sizes
+    };
+
+    //STREAM 0 STARTS HERE
+    //TODO: Parse each stream
+
+    //let mut buf = [0; 100];
+    //file.read_exact(&mut buf);
+    //let key = String::from_utf8(buf.to_vec())?;
+    //println!("{:#?}", key);
+    //let file_size = file.seek(SeekFrom::End(0))?;
+
+    Ok(())
+}
